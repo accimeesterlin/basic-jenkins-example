e8257c3425dc401a97634b1b64ed8081a760894d
Esterling Accime <esterling.accime@coxautoinc.com>
2019-04-14 19:15:39 -0400

    adding express static

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const { log } = console;
 
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
   log('Server is starting at ', PORT);
 });
